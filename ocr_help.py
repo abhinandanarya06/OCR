cc48c9495490094e7181f307f9a7b1891ef7c661
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2020-04-14 10:15:44 +0530

    Added Detailed Comments

--- ocr_help.py
+++ ocr_help.py
@@ -17,11 +17,13 @@ W = 0 # width of the character
 H = 1 # height of the character
 
 
-'''
-    "check_in(c, region)" checks if character is present in the given region
-    "region" of the image space
-'''
 def check_in(c, region):
+    '''
+Purpose: Check if character "c" present in word region "region"
+Input  : 1) character "c" with its position and shape-size
+         2) "region" with its rectangular vertices coordinates
+Output : return True if "c" is present in "region" else False
+    '''
     x, y, w, h = region
     center_x = c[POS][X]+c[SHAPE][W]/2
     center_y = c[POS][Y]
@@ -30,131 +32,148 @@ def check_in(c, region):
     return False
 
 
-'''
-    "get_region(c, regions)" finds the region in the given list of regions variable
-    "regions" in image space and return it
-'''
 def get_region(c, regions):
+    '''
+Purpose : Get one region from list of regions "regions" where character "c" is present
+Input   : 1) character "c" with its position and shape-size
+          2) list of regions "regions" each having its rectangular vertices coordinates
+Output  : Return the region from list of regions where c is belonging
+            if region not found, return False
+Approach: Linear Search
+    '''
     for region in regions:
         if check_in(c, region):
           return region
     return False
 
-'''
-    "sort_chars(line)" Function to sort characters in given line
-'''
 def sort_chars(line):
-  res = list()
-  while len(line) > 0:
-    mx = 100000
-    m = 0
-    for c in line:
-      if c[POS][X] <= mx:
-        mx = c[POS][X]
-        m = c
-    line.remove(m)
-    res.append(m)
-  return res
+    '''
+Purpose : Sorts characters in a line (list of characters)
+Input   : "line" list of characters
+Output  : "res" sorted list of characters
+Approach: Selection Sort
+    '''
+    res = list()
+    while len(line) > 0:
+        mx = 100000
+        m = 0
+        for c in line:
+            if c[POS][X] <= mx:
+                mx = c[POS][X]
+                m = c
+        line.remove(m)
+        res.append(m)
+    return res
 
 
-'''
-    "sort_lines_by_yval(lines)" function sorts list of lines according to its
-    y coordinate in image space
-'''
 def sort_lines_by_yval(lines):
-  res = list()
-  while len(lines) > 0:
+    '''
+Purpose : Sort the line by its y coordinate value
+Input   : "lines" list of lines
+Output  : "res" sorted list of lines
+Approach: Selection Sort
+    '''
+    res = list()
+    while len(lines) > 0:
     mn = 100000
     m = 0
     for line in lines:
-      if line[0][POS][Y] < mn:
-        mn = line[0][POS][Y]
-        m = line
+        if line[0][POS][Y] < mn:
+            mn = line[0][POS][Y]
+            m = line
     lines.remove(m)
     res.append(m)
-  return res
+    return res
 
 
-'''
-"arrangeline(characters)" Function to group characters in given image by lines
-'''
 def group_chars_by_line(characters):
-  lines = list()
-  linei = 0
-  while len(characters) > 0:
-      m = characters[0]
-      my = m[1][1]
-      my_plus_h = m[1][1]+m[2][1]
-      lines.append([m])
-      for c in characters[1:]:
-          if my <= c[POS][Y]+c[SHAPE][H]/2 and c[POS][Y]+c[SHAPE][H]/2 <= my_plus_h:
-              if my > c[POS][Y]:
-                my = c[POS][Y]
-              if my_plus_h < c[POS][Y]+c[SHAPE][H]:
-                my_plus_h = c[POS][Y]+c[SHAPE][H]
-              lines[linei].append(c)
-              characters.remove(c)
+    '''
+Purpose : Group text characters by lines
+Input   : "characters" list of characters with its position and height given in list itself
+Output  : "lines" list of lines itself list of characters
+Approach: Linear Search for each line
+    '''
+    lines = list()
+    linei = 0
+    while len(characters) > 0:
+        m = characters[0]
+        my = m[1][1]
+        my_plus_h = m[1][1]+m[2][1]
+        lines.append([m])
+        for c in characters[1:]:
+            if my <= c[POS][Y]+c[SHAPE][H]/2 and c[POS][Y]+c[SHAPE][H]/2 <= my_plus_h:
+                if my > c[POS][Y]:
+                    my = c[POS][Y]
+                if my_plus_h < c[POS][Y]+c[SHAPE][H]:
+                    my_plus_h = c[POS][Y]+c[SHAPE][H]
+                lines[linei].append(c)
+                characters.remove(c)
       lines[linei]= sort_chars(lines[linei])
       linei += 1
       characters.remove(m)
-  return lines
+    return lines
 
 '''
     "apply_ocr(img)" is the function that does actual ocr
     operation on image parameter to get text
 '''
 def apply_ocr(img, text_detector):
-    avg_h = 0
-    character_list = list()
-    color = img
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    '''
+Purpose: Apply OCR operation to extract text from given image
+Input  : 1) "img" image given from which text to be extracted
+         2) "text_detector" model which identifies the text on image
+Output : "TEXT" string text which will extracted from the "img" image
+    '''
+    avg_text_height = 0 # will be assigned with Average Height of text character
+    character_list = list() # will contain necessary text character to be extracted
     img = cv2.medianBlur(img,5)
     img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-            cv2.THRESH_BINARY,21,10)
+            cv2.THRESH_BINARY,21,10) # will remove maximum noise and make image suitable for contour dt
     contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    img[:,:]=0
+    black_map = img.copy()
+    black_map[:, :] = 0
     i = 0
-    for c in contours:
-      try:
-        x,y,w,h = cv2.boundingRect(c)
+    for contour in contours:
         try:
-            show = color[y-2:y+h+2, x-2:x+w+2]
+            x,y,w,h = cv2.boundingRect(contour)
+            try:
+                cnt = img[y-2:y+h+2, x-2:x+w+2]
+            except:
+                cnt = img[y:y+h, x:x+w]
+            cnt = cv2.resize(cnt, (30, 30), interpolation = cv2.INTER_AREA)
+            cnt = cnt.reshape((1, 30, 30, 1))
+            class_pred = np.argmax(text_detector.predict(cnt))
+            if class_pred < 40:
+                avg_text_height += h
+                cv2.rectangle(black_map, (x,y), (x+w+w//4, y+h), 255, -1)
+                character_list.append([keywords[class_pred], (x,y), (w,h)])
+                i += 1
         except:
-            show = color[y:y+h, x:x+w]
-        test_data = cv2.resize(show,(50,50),interpolation = cv2.INTER_AREA)
-      except:
-        continue
-      test_data=np.array([test_data])
-      cmd = np.argmax(text_detector.predict(test_data))
-      if cmd < 40:
-          avg_h += h
-          cv2.rectangle(img, (x,y), (x+w+w//4, y+h), 255, -1)
-          character_list.append([keywords[cmd], (x,y), (w,h)])
-          i+= 1
-    avg_h/=i
-    text = group_chars_by_line(character_list)#, img)
+              continue
+    avg_text_height /= i # Average Height of text character
+    text = group_chars_by_line(character_list)
     text = sort_lines_by_yval(text)
-    contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    regions = list()
-    for cnt in contours:
-      region = cv2.boundingRect(cnt)
-      if region[3] <= avg_h*7:
-        regions.append(region)
+    contours, hierarchy = cv2.findContours(black_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    regions = list() # will contain regions of words for grouping chars by word
+    for contour in contours:
+        region = cv2.boundingRect(contour)
+        if region[SHAPE + H] <= avg_text_height*7:
+          regions.append(region)
     del contours
 
     TEXT = ''
     for l in text:
-      char = l[0]
-      region = get_region(char, regions)
-      if not region:
-        continue
-      for char in l:
-        if not check_in(char, region):
-          TEXT = TEXT + ' '
-          r = get_region(char, regions)
-          if not r:
+        char = l[0]
+        region = get_region(char, regions)
+        if not region:
             continue
-          region = r
-        TEXT = TEXT + char[0]
-      TEXT = TEXT + '\n'
+        for char in l:
+            if not check_in(char, region):
+                TEXT = TEXT + ' '
+                r = get_region(char, regions)
+                if not r:
+                    continue
+                region = r
+            TEXT = TEXT + char[0]
+        TEXT = TEXT + '\n'
     return TEXT
